0d2e327f0ed9f2012ab4be5b876d3227b4c43781
saulpark <aks_@naver.com>
2021-02-23 01:38:43 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
-    console.log(window.scrollY);
-    console.log(navbarHeight);
-
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else{
@@ -13,3 +11,17 @@ document.addEventListener('scroll', () =>{
     }
 
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
